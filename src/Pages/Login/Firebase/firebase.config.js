222c3e2bea01b12780aef02067888ee5cb63e43b
Nasif Zeehan <nasifzeehan1@gmail.com>
2021-10-27 17:22:45 +0600

    full push

---
+++ src/Pages/Login/Firebase/firebase.config.js
@@ -0,0 +1,11 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBcWzfq_G0aNi0TudEgrr0fY_EZGXDqCMM",
+    authDomain: "react-website-483b8.firebaseapp.com",
+    projectId: "react-website-483b8",
+    storageBucket: "react-website-483b8.appspot.com",
+    messagingSenderId: "320286947103",
+    appId: "1:320286947103:web:7e2f342f078c4b4668a22a"
+};
+
+export default firebaseConfig;
