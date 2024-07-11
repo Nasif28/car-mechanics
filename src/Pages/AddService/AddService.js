97beed0e1d99e536e29940a6added8218fa10472
Nasif Jihan <nasifzeehan1@gmail.com>
2024-07-11 17:12:24 +0600

    server link added

--- src/Pages/AddService/AddService.js
+++ src/Pages/AddService/AddService.js
@@ -7,7 +7,7 @@ const AddService = () => {
     const { register, handleSubmit, reset } = useForm();
     const onSubmit = data => {
         console.log(data);
-        axios.post('http://localhost:5000/services', data)
+        axios.post('https://car-mechanics-server-ef4b.onrender.com/services', data)
             .then(res => {
                 if (res.data.insertedId) {
                     alert('added successfully');
--- src/Pages/Booking/Booking/Booking.js
+++ src/Pages/Booking/Booking/Booking.js
@@ -6,7 +6,7 @@ const Booking = () => {
     const [service, setService] = useState({});
 
     useEffect(() => {
-        fetch(`http://localhost:5000/services/${serviceId}`)
+        fetch(`https://car-mechanics-server-ef4b.onrender.com/services/${serviceId}`)
             .then(res => res.json())
             .then(data => setService(data));
     }, [])
